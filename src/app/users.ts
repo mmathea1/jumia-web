3f0d8e2a7cbef5fcd78626ea63ea48b64f4c6dfb
mingina <mathea.mingina@busaracenter.org>
2023-01-25 14:17:37 +0300

    mapping api call results to user data type

--- src/app/users.ts
+++ src/app/users.ts
@@ -2,16 +2,9 @@ export interface User {
     id: number;
     email: string;
     name: string;
-
+    gender: string;
+    location: string;
+    age: number;
+    registered: number;
+    phone: string;
 }
-export const USERS: User[] = [
-    { id: 10, email: '12@gmail.com', name: 'Dr. Nice' },
-    { id: 11, email: '13@gmail.com', name: 'Bombasto' },
-    { id: 12, email: '14@gmail.com', name: 'Celeritas' },
-    { id: 13, email: '15@gmail.com', name: 'Magneta' },
-    { id: 14, email: '16@gmail.com', name: 'RubberMan' },
-    { id: 15, email: '17@gmail.com', name: 'Dynama' },
-    { id: 16, email: '18@gmail.com', name: 'Dr. IQ' },
-    { id: 17, email: '19@gmail.com', name: 'Magma' },
-    { id: 18, email: '20@gmail.com', name: 'Tornado' }
-];
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ExportService } from '../services/export.service';
+import { User } from '../users';
 
 
 
@@ -14,8 +15,8 @@ import { ExportService } from '../services/export.service';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  dataSource = new MatTableDataSource<[]>();
-  displayedColumns: string[] = ['name', 'email', 'gender', 'location', 'age', 'registration', 'phone_number', 'actions'];
+  dataSource = new MatTableDataSource<User[]>();
+  displayedColumns: string[] = ['name', 'email', 'gender', 'location', 'age', 'registered', 'phone', 'actions'];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -34,15 +35,30 @@ export class UsersComponent implements OnInit {
   getUsers(): void {
     this.usersService.getUsers()
       .pipe((map(users => {
-        this.dataSource.data = users['results'];
+        var res = users['results'];
+        var data: any = [];
+        for (var i = 0; i < res.length; i++) {
+          var user: User = {
+            id: res[i]['id'].value,
+            name: res[i]['name'].first + ' ' + res[i]['name'].last,
+            email: res[i]['email'],
+            gender: res[i]['gender'],
+            location: res[i]['location'].country,
+            age: res[i]['dob'].age,
+            registered: res[i]['registered'].age,
+            phone: res[i]['phone']
+          };
+          data.push(user);
+        }
+
+        this.dataSource.data = data;
       })))
       .subscribe();
-
   }
 
   exportFile(exportType: string): void {
-    var date = new Date().toLocaleString();
-    var fileName = 'users' + date;
+    var date = new Date().toJSON().slice(0, 10).toString();
+    var fileName = 'users_' + date;
     if (exportType === 'xml') {
       this.exportService.exportFile(this.dataSource.data, fileName, 'xml', this.displayedColumns);
     } else {
