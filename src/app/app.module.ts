2ef44850600c37ddd7e83205e437a7ae50b82851
mingina <mathea.mingina@busaracenter.org>
2023-01-25 20:46:53 +0300

    added bootstrap, able to select users with mat-check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './users/users.component';
 import { MatTableModule } from '@angular/material/table';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @NgModule({
   declarations: [
@@ -26,7 +28,12 @@ import { MatSortModule } from '@angular/material/sort';
     FormsModule,
     HttpClientModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,7 +6,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { ExportService } from '../services/export.service';
 import { User } from '../users';
+import { SelectionModel } from '@angular/cdk/collections';
 
+const allowMultiSelect = true;
 
 
 @Component({
@@ -15,10 +17,17 @@ import { User } from '../users';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  dataSource = new MatTableDataSource<User[]>();
-  displayedColumns: string[] = ['name', 'email', 'gender', 'location', 'age', 'registered', 'phone', 'actions'];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  dataSource = new MatTableDataSource<User[]>();
+  displayedColumns: string[] = ['select', 'name', 'email', 'gender', 'location', 'age', 'registered', 'phone', 'view_user'];
+  selection = new SelectionModel<any>(allowMultiSelect, []);
+  pageSize: number = 50;
+  pageSizeOptions: number[] = [50, 100, 250, 500];
+  pageLength: number = 50;
+
+
+
 
   constructor(private usersService: UserService, private exportService: ExportService) { }
 
@@ -59,12 +68,25 @@ export class UsersComponent implements OnInit {
   exportFile(exportType: string): void {
     var date = new Date().toJSON().slice(0, 10).toString();
     var fileName = 'users_' + date;
+    var data = this.selection.selected.length > 0 ? this.selection.selected : this.dataSource.data;
     if (exportType === 'xml') {
-      this.exportService.exportFile(this.dataSource.data, fileName, 'xml', this.displayedColumns);
+      this.exportService.exportFile(data, fileName, 'xml', this.displayedColumns);
     } else {
-      this.exportService.exportFile(this.dataSource.data, fileName, 'csv', this.displayedColumns);
+      this.exportService.exportFile(data, fileName, 'csv', this.displayedColumns);
     }
   }
+
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  toggleAllRows(): void {
+    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row))
+  }
+
+
 
 
 
